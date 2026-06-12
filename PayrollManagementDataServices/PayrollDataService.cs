1b089a3c4aed149ea692d2ac18bd388688e7ce52
Marie Thea Dela Cruz <marietheadelacruz@gmail.com>
2026-06-12 12:59:45 +0800

    Initial commit

---
+++ PayrollManagementDataServices/PayrollDataService.cs
@@ -0,0 +1,32 @@
+﻿using PayrollManagementModels;
+
+namespace PayrollManagementDataServices
+{
+    public class PayrollDataService
+    {
+        IEmployeeDataService dataservice;
+        public PayrollDataService(IEmployeeDataService iEmployeeDataService)
+        {
+            dataservice = iEmployeeDataService;
+        }
+        public void Add(Employee employ)
+        {
+            dataservice.Add(employ);
+        }
+
+        public List<Employee> GetEmployee()
+        {
+            return dataservice.GetEmployee();
+        }
+
+        public void UpdateEmployee(Employee employee)
+        {
+            dataservice.UpdateEmployee(employee);
+        }
+        public Employee? SearchByEmployee(Guid id)
+        {
+            return dataservice.SearchEmployee(id);
+        }
+
+    }
+}
---
+++ PayrollManagementDataServices/PayrollMemoryData.cs
@@ -0,0 +1,54 @@
+﻿using PayrollManagementModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PayrollManagementDataServices
+{
+    public class PayrollMemoryData
+    {
+        private List<Employee> employees = new List<Employee>();
+
+        public PayrollMemoryData()
+        {
+            employees.Add(new Employee
+            {
+                EmployeeId = Guid.NewGuid(),
+                Name = "Employee A",
+                Position = "Crew",
+                DaysWorked = 12,
+                OvertimeHours = 2,
+                DailyRate = 600,
+                OvertimeRate = 92
+            });
+
+            employees.Add(new Employee
+            {
+                EmployeeId = Guid.NewGuid(),
+                Name = "Employee B",
+                Position = "Crew",
+                DaysWorked = 10,
+                OvertimeHours = 3,
+                DailyRate = 600,
+                OvertimeRate = 92
+            });
+        }
+
+        public List<Employee> GetEmployees()
+        {
+            return employees;
+        }
+
+        public Employee GetEmployeeById(Guid id)
+        {
+            return employees.FirstOrDefault(e => e.EmployeeId == id);
+        }
+
+        public void AddEmployee(Employee emp)
+        {
+            employees.Add(emp);
+        }
+    }
+}
